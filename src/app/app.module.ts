21b765e04996216dea76e7424acccf4377f5cb64
root <phibos@gmail.com>
2019-12-05 19:10:33 +0000

    modified:   angular.json 	new file:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PoModule } from '@portinari/portinari-ui';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PoModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
